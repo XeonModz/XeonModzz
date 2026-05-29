a55ec7b7119eca96cd52d69e3b56ddef4283d468
XeonModz <hrithikuday777@gmail.com>
2026-05-29 12:29:50 +0530

    Fixed config merge

--- xeonmodz/lib/mode.py
+++ xeonmodz/lib/mode.py
@@ -1,6 +1,8 @@
 # Version: 1.0 Beta
 # ©️ 2025 xeonmodz ALL RIGHTS RESERVED
-from pyrogram import Client, filters
+
+from functools import wraps
+from pyrogram import Client
 from pyrogram.types import Message
 from config import SUDO, MODE
 
@@ -8,14 +10,18 @@ from config import SUDO, MODE
 def is_admin(user_id: int) -> bool:
     return user_id in SUDO
 
+
 def isPrivate(func):
+    @wraps(func)
     async def wrapper(client: Client, message: Message):
-        if MODE.lower() == 'public' or is_admin(message.from_user.id):
-            await func(client, message)
-        else:
-            await message.reply("This bot is private and restricted to SUDO only.")
+        if MODE.lower() == "public":
+            return await func(client, message)
+
+        if message.from_user and is_admin(message.from_user.id):
+            return await func(client, message)
+
+        return await message.reply_text(
+            "This bot is private and restricted to SUDO only."
+        )
+
     return wrapper
-
-
-
-
